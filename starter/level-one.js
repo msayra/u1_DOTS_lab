51750a06c7b60ac07342c792b41de969708704eb
msayra <msayra333@gmail.com>
2024-09-24 13:00:34 -0500

    completed and ready for submission

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,11 @@
  * DOTS: Level One
  *
  */
+function switchColor(colors) {
+    randomColor = Math.floor(Math.random() * colors.length)
+    document.body.style.backgroundColor = colors[randomColor]
+    randomColor = Math.floor(Math.random() * colors.length)
+    for (ball of balls) {
+        ball.style.backgroundColor = colors[randomColor]
+    }
+}
